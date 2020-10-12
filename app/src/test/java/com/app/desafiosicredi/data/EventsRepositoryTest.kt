9cff95d3c9792477bea56efed2f00209dfd5a502
unknown <aavila.bio@gmail.com>
2020-10-12 19:21:56 -0300

    implement events repository tests

--- app/src/test/java/com/app/desafiosicredi/data/EventsRepositoryTest.kt
+++ app/src/test/java/com/app/desafiosicredi/data/EventsRepositoryTest.kt
@@ -1,5 +1,8 @@
 package com.app.desafiosicredi.data
 
+import com.app.desafiosicredi.data.eventdetail.model.CheckinRequestBody
+import com.app.desafiosicredi.data.eventdetail.model.CheckinResponse
+import com.app.desafiosicredi.data.eventdetail.model.EventDetail
 import com.app.desafiosicredi.data.events.api.EventsApiService
 import com.app.desafiosicredi.data.events.model.Events
 import com.app.desafiosicredi.data.events.model.EventsItem
@@ -18,6 +21,7 @@ class EventsRepositoryTest {
     private val eventsApi = mockk<EventsApiService>(relaxed = true)
     private val repository = EventsRepository(eventsApi)
 
+    private val eventId = "1"
     private val eventsExpected: Events = Events()
     private val eventItem = EventsItem(
         1602296327,
@@ -30,29 +34,59 @@ class EventsRepositoryTest {
         30.5,
         "Teste"
     )
-
-    @Test
-    fun mustCallApi() = runBlocking {
-        mockApiSuccess()
-
-        repository.getEvents()
-
-        coVerify { eventsApi.getEvents() }
-    }
+    private val eventDetail = EventDetail(
+        1602296327,
+        "Evento",
+        "1",
+        "image.png",
+        20.80,
+        52.25,
+        listOf(),
+        30.5,
+        "Teste"
+    )
+    private val checkinData = CheckinRequestBody(
+        "Fulano Silva",
+        "teste@teste.com",
+        "1"
+    )
+    private val successCode = CheckinResponse("200")
 
     @Test
     fun onSuccess_mustReturnEvents() = runBlocking {
-        mockApiSuccess()
+        mockGetEventsSuccess()
 
         val events = repository.getEvents()
 
         coVerify { eventsApi.getEvents() }
 
-        assertThat(eventsExpected).isEqualTo(events)
+        assertThat(setNetworkSuccess(eventsExpected)).isEqualTo(events)
     }
 
     @Test
-    fun whenOrderNotFound_mustReturnNull() = runBlocking {
+    fun onSuccess_mustReturnEventDetail() = runBlocking {
+        mockGetEventDetailSuccess()
+
+        val eventDetailResponse = repository.getEventDetail(eventId)
+
+        coVerify { eventsApi.getEventDetail(eventId) }
+
+        assertThat(setNetworkSuccess(eventDetail)).isEqualTo(eventDetailResponse)
+    }
+
+    @Test
+    fun onSuccess_mustReturnCheckinConfirmation() = runBlocking {
+        mockMakeCheckinSuccess()
+
+        val checkinResponse = repository.makeCheckin(checkinData)
+
+        coVerify { eventsApi.makeCheckin(checkinData) }
+
+        assertThat(setNetworkSuccess(successCode)).isEqualTo(checkinResponse)
+    }
+
+    @Test
+    fun whenResponseNotFound_mustReturnNull() = runBlocking {
         mockApiError()
 
         val events = repository.getEvents()
@@ -62,7 +96,7 @@ class EventsRepositoryTest {
         assertThat(events).isNull()
     }
 
-    private fun mockApiSuccess() {
+    private fun mockGetEventsSuccess() {
         eventsExpected.add(eventItem)
 
         coEvery { eventsApi.getEvents() }.returns(
@@ -70,13 +104,28 @@ class EventsRepositoryTest {
         )
     }
 
+    private fun mockGetEventDetailSuccess() {
+        coEvery { eventsApi.getEventDetail(eventId) }.returns(
+            NetworkResponse.Success(eventDetail, code = 200)
+        )
+    }
+
+    private fun mockMakeCheckinSuccess() {
+        coEvery { eventsApi.makeCheckin(checkinData) }.returns(
+            NetworkResponse.Success(successCode, code = 200)
+        )
+    }
+
     private fun mockApiError() {
         eventsExpected.add(eventItem)
 
         coEvery { eventsApi.getEvents() }.returns(
-            NetworkResponse.ServerError("error".toResponseBody("application/json".toMediaTypeOrNull()), code = 400)
+            NetworkResponse.ServerError(
+                "error".toResponseBody("application/json".toMediaTypeOrNull()),
+                code = 400
+            )
         )
     }
 
-
+    private fun setNetworkSuccess(item: Any) = NetworkResponse.Success(item, code = 200)
 }
